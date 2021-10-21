53a18e5af87321c149931ef0854d07ff7fea667f
Tse018 <thanushan.s@hotmail.com>
2021-10-21 13:33:00 +0200

    Some errors needs to be fixed

--- js/app.js
+++ js/app.js
@@ -1,186 +1,165 @@
-const section = document.querySelector("section");
-const showCartButton = document.getElementById("cart_items");
-
-const sectionState = {
-   isVisible: false,
-   isInvinsible: true,
-};
-
-function updateView() {
-   if (sectionState.isVisible === true) {
-      section.classList.remove("product_list");
-   } else {
-      section.classList.add("product_list");
-   }
-}
-
-function onMenuButtonClick() {
-   sectionState.isVisible = !sectionState.isVisible;
-   updateView();
-}
-
-showCartButton.addEventListener("click", onMenuButtonClick);
-
-
-// liste med alle produkter lagt til i handlekurven
 /***************** Model ******************************/
 const products = {
    productsInformation: [
       {
          id: 0,
-         name: "Iphone 13",
-         description: "productDescription",
-         price: "12014",
-         img: "assets/iphone 13.jpg"
+         name: "Iphone 13 Pro",
+         description: "Iphone 13 Pro.",
+         price: "12014,-",
+         img: "assets/iphone 13 Pro.jpg"
       },
       {
          id: 1,
          name: "Samsung Galaxy Z fold3",
          description: "productDescription",
          price: 324423,
-         img: "assets/iphone 13.jpg"
+         img: "assets/samsung.jpg"
       },
       {
          id: 2,
-         name: "iMac",
+         name: "Iphone 13",
          description: "productDescription",
          price: 3244,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Iphone 13.jpg"
       },
       {
          id: 3,
          name: "Apple Watch",
          description: "productDescription",
          price: 32432,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Apple Watch.jpg"
       },
       {
          id: 4,
-         name: "productName",
+         name: "Samsung2",
          description: "productDescription",
          price: 32423,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Samsung2.jpg"
       },
       {
          id: 5,
-         name: "productName",
+         name: "MacBook Air",
          description: "productDescription",
          price: 2443,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Macbook Air.jpg"
       },
       {
          id: 6,
-         name: "productName",
+         name: "MacBook Pro",
          description: "productDescription",
          price: 3424,
-         img: "assets/iphone 13.jpg"
+         img: "assets/MacBook Pro.jpg"
       },
       {
          id: 7,
-         name: "productName",
+         name: "Bærbar PC",
          description: "productDescription",
          price: 3452,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Bærbar.jpg"
       },
       {
          id: 8,
-         name: "productName",
+         name: "Gaming pc",
          description: "productDescription",
          price: 4323,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Gaming PC-1.jpg"
       },
       {
          id: 9,
-         name: "productName",
+         name: "Playstation 5",
          description: "productDescription",
          price: 4759,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Playstation 5.jpg"
       },
       {
          id: 10,
-         name: "productName",
+         name: "Gaming skjerm",
          description: "productDescription",
          price: 8927,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Gaming skjerm.jpg"
       },
       {
          id: 11,
-         name: "productName",
+         name: "Gaming skjerm",
          description: "productDescription",
          price: 7389,
-         img: "assets/iphone 13.jpg"
+         img: "assets/Gaming skjerm 2.jpg"
       },
       {
          id: 12,
-         name: "productName",
+         name: "Gaming Stol 1",
          description: "productDescription",
          price: 6734,
-         img: ""
+         img: "assets/Gaming Stol 1.jpg"
       },
       {
          id: 13,
-         name: "productName",
+         name: "Gaming Stol 2",
          description: "productDescription",
          price: 6483,
-         img: ""
+         img: "assets/Gaming Stol 2.jpg"
       },
       {
          id: 14,
-         name: "productName",
+         name: "Gaming Stol 3",
          description: "productDescription",
          price: 1849,
-         img: ""
+         img: "assets/Gaming Stol 3.jpg"
       },
       {
          id: 15,
          name: "productName",
          description: "productDescription",
          price: 4852,
-         img: ""
+         img: "assets/klokke.jpg"
       },
       {
          id: 16,
-         name: "productName",
+         name: "Hodetelefon",
          description: "productDescription",
          price: 7832,
-         img: ""
+         img: "assets/Hodetelefoner.jpg"
       },
       {
          id: 17,
-         name: "productName",
+         name: "Gaming Mus",
          description: "productDescription",
          price: 7299,
-         img: ""
+         img: "assets/Gaming mus 2.jpg"
       },
       {
          id: 18,
-         name: "productName",
+         name: "Gaming Tastatur",
          description: "productDescription",
          price: 9128,
-         img: ""
+         img: "assets/Tastatur.jpg"
       },
       {
          id: 19,
-         name: "productName",
+         name: "Expensive TV",
          description: "productDescription",
-         price: 2138,
-         img: ""
+         price: 81038,
+         img: "assets/insane TV.jpg"
       },
       {
          id: 20,
-         name: "productName",
+         name: "Insane PC",
          description: "productDescription",
-         price: 4939,
-         img: ""
+         price: 940939,
+         img: "assets/insane PC.jpg"
       }
    ]
 };
 
 /************** Controllers *********************/
-
+// legge til produkter i en ny liste itemsInCart
 let itemsInCart = []
 
-// legge til produkter i listen itemsInCart
+/* funksjon som skal sjekke om produktet finnes i listen itemsInCart,
+   hvis det finnes, så økes antallet med 1,
+   hvis det ikke finnes skal det finne produkt id og legge til i itemsInCart
+*/
+
 function addToCart(id) {
    if (itemsInCart.some((item) => item.id === id)) {
       changeNumbersOfSameProduct("plus", id);
@@ -194,14 +173,16 @@ function addToCart(id) {
          numberOfUnits: 1,
       });
    }
-   updateCart();
+   updateCart(itemsInCart);
 }
 
+// fjern item fra listen
 function removeItemFromCart(id) {
-   itemsInCart = itemsInCart.filter((item) => item.id !== id);
-   updateCart();
+   itemsInCart = itemsInCart.filter((item) => item.id !== id); // NOT equal value / NOT equal value type
+   updateCart(itemsInCart);
 }
 
+// teller total pris og total produkter lagt til i handlekurven
 function countItemsAddedToCart() {
    let totalPrice = 0,
    totalItems = 0;
@@ -211,6 +192,9 @@ function countItemsAddedToCart() {
       totalItems += item.numberOfUnits;
    });
 }
+
+
+// når man legger til like produkter, så skal antallet økes med +1 eller ta vekk med -1.
 
 function changeNumbersOfSameProduct(event, id) {
    itemsInCart = itemsInCart.map((item) => {
@@ -229,12 +213,12 @@ function changeNumbersOfSameProduct(event, id) {
          numberOfUnits,
       };
    });
-   updateCart();
+   updateCart(itemsInCart);
 }
 
-function updateCart() {
-   updateCartView();
-   countItemsAddedToCart();
+function updateCart(itemsInCart) {
+   updateCartView(itemsInCart);
+   countItemsAddedToCart(itemsInCart);
 }
 
 
@@ -269,8 +253,8 @@ function product() {
       let addProductToCartButton = document.createElement("button");
       addProductToCartButton.className = "i fas fa-shopping-cart";
       addProductToCartButton.id = product.id, addToCart;
+      addProductToCartButton.dataset.productId = product.id;
       productSection.appendChild(addProductToCartButton);
-
       productItems.appendChild(productSection);
    });
 }
@@ -278,11 +262,10 @@ function product() {
 product();
 
 // oppdaterer handlelisten
-function updateCartView() {
+function updateCartView(itemsInCart) {
    const section = document.querySelector("section");
-
    section.innerHTML = "";
-   itemsInCart.forEach((item, updateCart ) => {
+   itemsInCart.forEach((item) => {
       let cartProducts = document.createElement("div");
       cartProducts.className = "cart_item";
 
@@ -300,34 +283,68 @@ function updateCartView() {
       cartProducts.appendChild(cartProductDescription);
 
       let cartProductQuantity = document.createElement('h3');
-      cartProductQuantity.innerText = item;
+      cartProductQuantity.innerText = item.numberOfUnits;
       cartProducts.appendChild(cartProductQuantity)
 
       let cartProductPrice = document.createElement("h3");
-      cartProductPrice.innerText = item;
+      cartProductPrice.innerText = item.price;
       cartProducts.appendChild(cartProductPrice);
 
       let dangerButton = document.createElement("button");
       dangerButton.className = "i fas fa-trash-alt";
       dangerButton.id = item, removeItemFromCart;
+      dangerButton.dataset.remove = dangerButton.id;
       cartProducts.appendChild(dangerButton);
 
       section.appendChild(cartProducts);
    });
 }
 
+updateCartView(itemsInCart);
+
 /************************************** Add eventlistener ***************************/
 
-let buttons = document.querySelectorAll(".fa-shopping-cart");
+// legger til eventlistener for kjøp-produkt knappen som trigger funksjonen addToCart()
+let buttons = document.querySelector(".fa-shopping-cart");
 
-buttons.forEach((buttons) => {
-   buttons.addEventListener("click", addToCart);
+// finn hver knapp og legg til produktet i handlevognen og til slutt oppdater handlekurven
+buttons.forEach((event) => {
+   const productId = event.currentTarget.dataset.productId;
+   buttons.addEventListener('click', addToCart(productId));
+   updateCart(itemsInCart);
 });
 
+// legger til eventlistener for danger-knappen som trigger funksjonen removeItemFromCart()
 let removeButton = document.querySelectorAll(".fa-trash-alt");
 
-removeButton.forEach((removeButton) => {
-   removeButton.addEventListener("click", removeItemFromCart);
+removeButton.forEach((event) => {
+   const removeProduct = event.currentTarget.dataset.remove;
+   event.addEventListener("click", removeItemFromCart(removeProduct));
+   updateCart(itemsInCart);
 });
 
+/*************** Show / Hide Cart ***************/
+// må integrere denne delen med hele modellen
 
+const section = document.querySelector("section");
+const showCartButton = document.getElementById("cart_items");
+
+const sectionState = {
+   isVisible: false,
+   isInvinsible: true,
+};
+
+function updateView() {
+   if (sectionState.isVisible === true) {
+      section.classList.remove("product_list"); // legger til og tar vekk klassen
+   } else {
+      section.classList.add("product_list");
+   }
+}
+
+function onMenuButtonClick() {
+   sectionState.isVisible = !sectionState.isVisible;
+   updateView();
+}
+
+showCartButton.addEventListener("click", onMenuButtonClick);
